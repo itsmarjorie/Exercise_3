4631b06a10758e5274cecd70e87cced876b63bb4
MARJORIE MANALO <mrbmanalo@paterostechnologicalcollege.edu.ph>
2026-03-11 20:47:18 +0800

    Create grp3_manalo_ex2.py

---
+++ grp3_manalo_ex2.py
@@ -0,0 +1,36 @@
+‎# My first simple script
+‎
+‎# Simple print script with fullname, course, year and section
+‎print("Hello, I'm Marjorie Manalo! BSIT-1V")
+‎
+‎# Python Variables
+‎
+‎#Create two variables and assign a number and name on it.
+‎num = 19
+‎name = "Marjorie"
+‎
+‎print("Number:", num)
+‎print("Name:", name)
+‎
+‎# Python Variable Names (example first names of group members)
+‎nicole = "Nicole"
+‎joanne = "Joanne"
+‎riza = "Riza"
+‎lourenz = "Lourenz"
+‎
+‎print("nicole")
+‎print("joanne")
+‎print("riza")
+‎print("lourenz")
+‎
+‎# Many Values to Multiple Variables
+‎a, b, c = 10, 20, 30
+‎print(a)
+‎print(b)
+‎print(c)
+‎
+‎# One Value to Multiple Variables
+‎x = y = z = 500
+‎print(x)
+‎print(y)
+‎print(z)
