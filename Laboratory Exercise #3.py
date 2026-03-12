9988c3bf458a41ba8f25f7d67ef5cf4f169c2aef
MARJORIE MANALO <mrbmanalo@paterostechnologicalcollege.edu.ph>
2026-03-12 11:04:45 +0800

    Add files via upload

---
+++ Laboratory Exercise #3.py
@@ -0,0 +1,36 @@
+# My first simple script
+
+# Simple print script with fullname, course, year and section
+print("Hello, I'm Marjorie Manalo! BSIT-1V")
+
+# Python Variables
+
+#Create two variables and assign a number and name on it.
+num = 19
+name = "Marjorie"
+
+print("Number:", num)
+print("Name:", name)
+
+# Python Variable Names (example first names of group members)
+nicole = "Nicole"
+joanne = "Joanne"
+riza = "Riza"
+lourenz = "Lourenz"
+
+print("nicole")
+print("joanne")
+print("riza")
+print("lourenz")
+
+# Many Values to Multiple Variables
+a, b, c = 10, 20, 30
+print(a)
+print(b)
+print(c)
+
+# One Value to Multiple Variables
+x = y = z = 500
+print(x)
+print(y)
+print(z)
